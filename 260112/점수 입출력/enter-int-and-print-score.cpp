39a507e2f5b5549259defc8873c962fd55c6ecd1
Wooseohyun <woosh0901@gmail.com>
2026-01-12 10:18:18 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "Your score is " << n << " point.";
+    // Please write your code here.
+    return 0;
+}
