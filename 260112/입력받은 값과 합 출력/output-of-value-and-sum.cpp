abf8c79d858908e193d95b950fe7ebf858aedea4
Wooseohyun <woosh0901@gmail.com>
2026-01-12 10:28:20 +0900

    Title: 입력받은 값과 합 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a>> b;
+    cout << a << " " << b << " " << a+b;
+    // Please write your code here.
+    return 0;
+}
