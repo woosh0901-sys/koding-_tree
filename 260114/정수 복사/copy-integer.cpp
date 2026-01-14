4beb401b09d6f340e0410e825e8e5077414b2047
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:55:19 +0900

    Title: 정수 복사, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/정수 복사/copy-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3,b=3;
+    int temp;
+    temp =a;
+    a = b;
+    cout << a << " " << b << "\n" << a *b;
+    // Please write your code here.
+    return 0;
+}
