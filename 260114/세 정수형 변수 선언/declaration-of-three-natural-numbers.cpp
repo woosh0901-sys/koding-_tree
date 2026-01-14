39fd9153b5c2b2d2eb07175000899d1f298399c6
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:43:06 +0900

    Title: 세 정수형 변수 선언, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 7,b =23 , c = 30;
+    cout << a << " + " << b << " = " << c;
+    // Please write your code here.
+    return 0;
+}
