a78659a868e36e843c4474b5357726c0c6a58bb7
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:54:05 +0900

    Title: 변수 값 교체하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/변수 값 교체하기 2/replacing-variable-values-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a =5;
+    int b = 3;
+    int temp;
+    temp =a;
+    a = b;
+    cout << a << "\n" << b;
+    // Please write your code here.
+    return 0;
+}
