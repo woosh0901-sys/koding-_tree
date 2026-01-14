f8d4c01921cef4225a1b40ea12ee09f1be11816d
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:47:42 +0900

    Title: 소수점 반올림하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 25.352;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << a;
+    // Please write your code here.
+    return 0;
+}
