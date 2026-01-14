e022b982445543adaf33efc714b5b99c0379ba82
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:37:46 +0900

    Title: 두줄 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello students!" << "\n";
+    cout << "Welcome to LeebrosCode!";
+    // Please write your code here.
+    return 0;
+}
