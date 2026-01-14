f4420edf1cbb3005de50c1513f1fe67acf04ba73
WooSeoHyun <woosh0901@gmail.com>
2026-01-14 13:59:43 +0900

    Title: 변수 값 복사하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    a =c,b=c;
+    cout << a << " " << b << " " << c;
+    // Please write your code here.
+    return 0;
+}
