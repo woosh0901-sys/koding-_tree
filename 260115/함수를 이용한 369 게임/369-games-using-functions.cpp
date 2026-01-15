c8692e7340b77627d18ea8364fea3ebdefc06d06
WooSeoHyun <woosh0901@gmail.com>
2026-01-15 13:52:25 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260115/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int check(int a, int b) {
+    int count = 0;
+    while (a <= b) {
+        int current = b;
+        while (current > 0) {
+            int digit = current % 10;     
+            if (digit == 3 || digit == 6 || digit == 9) {
+                count++;
+                cout << current << "\n";
+            }
+            current /= 10;
+        }
+        b--;
+    }
+    return count;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << check(a, b);
+    return 0;
+}
