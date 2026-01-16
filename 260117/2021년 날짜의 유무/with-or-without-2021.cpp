30b70a40b25585d59b9c6dcd46b01682762f9155
WooSeoHyun <woosh0901@gmail.com>
2026-01-17 08:56:22 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 260117/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
-bool check(int a, int b){
-    if(a<1 || a> 12) return false;
-    if(a == 1 && b>1 || b<32) return true
-    else false;
-    if(a == 1 && b>1 || b<32) return true
-    else false;
-    if(a == 1 && b>1 || b<32) return true
-    else false;
+
+bool check(int m, int d) {
+    if (m < 1 || m > 12) return false;
+
+    int last_days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    if (d >= 1 && d <= last_days[m]) {
+        return true;
+    }
+
+    return false;
 }
 
 int main() {
     int m,d;
     cin >> m >> d;
+
+    if(check(m,d))
+        cout << "Yes";
+    else
+        cout << "No";
     // Please write your code here.
     return 0;
 }
