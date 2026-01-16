24c1dc808475e4efe262c54386a1544db483fe56
WooSeoHyun <woosh0901@gmail.com>
2026-01-17 08:52:54 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260117/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+bool check(int a, int b){
+    if(a<1 || a> 12) return false;
+    if(a == 1 && b>1 || b<32) return true
+    else false;
+    if(a == 1 && b>1 || b<32) return true
+    else false;
+    if(a == 1 && b>1 || b<32) return true
+    else false;
+}
+
+int main() {
+    int m,d;
+    cin >> m >> d;
+    // Please write your code here.
+    return 0;
+}
