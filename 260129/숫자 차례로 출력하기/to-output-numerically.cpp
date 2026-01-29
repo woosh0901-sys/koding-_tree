a1a30cfde1a17dbe7c6c98e26cbd03e6d59c3c34
WooSeoHyun <woosh0901@gmail.com>
2026-01-29 11:34:27 +0900

    Title: 숫자 차례로 출력하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260129/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i =0 ;i<n;i++){
+        cout << i+1 << " ";
+    }
+    cout << "\n";
+    for(int i =7 ;i>0;i--){
+        cout << i << " ";
+    }
+    // Please write your code here.
+    return 0;
+}
