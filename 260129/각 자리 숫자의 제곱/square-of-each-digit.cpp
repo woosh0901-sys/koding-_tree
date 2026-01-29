d51fef1f845b5352105005b494e4911e2e2cbe2d
WooSeoHyun <woosh0901@gmail.com>
2026-01-29 16:29:21 +0900

    Title: 각 자리 숫자의 제곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    int k = 0;
+    while(n>0){
+        k = n %10;
+        n /= 10;
+        sum += k * k;
+    }
+    cout << sum;
+    // Please write your code here.
+    return 0;
+}
