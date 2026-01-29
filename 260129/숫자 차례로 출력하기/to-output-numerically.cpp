943a91c6ff4d0127461987a8cd614afbdfa9264d
WooSeoHyun <woosh0901@gmail.com>
2026-01-29 11:37:53 +0900

    Title: 숫자 차례로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260129/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 260129/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void check(int n) {
+    if (n == 0) return;
+
+    check(n - 1); 
+    cout << n << " ";
+}
+
+
+void check1(int n) {
+    if (n == 0) return;
+
+    cout << n << " ";
+    check1(n - 1);
+}
+
 int main() {
     int n;
-    cin >> n;
-    for(int i =0 ;i<n;i++){
-        cout << i+1 << " ";
-    }
+    cin >> n
+
+    check(n);
     cout << "\n";
-    for(int i =7 ;i>0;i--){
-        cout << i << " ";
-    }
-    // Please write your code here.
+    check1(n);
+
     return 0;
 }
