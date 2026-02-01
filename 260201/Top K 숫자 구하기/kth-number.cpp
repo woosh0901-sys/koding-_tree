5a652c55e6a82f12ef90e84b4423be0e704c4f8d
WooSeoHyun <woosh0901@gmail.com>
2026-02-01 09:21:32 +0900

    Title: Top K 숫자 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260201/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, k;
+int nums[1000];
+
+int main() {
+    cin >> N >> k;
+
+    for (int i = 0; i < N; i++) {
+        cin >> nums[i];
+    }
+    sort(nums,nums + N);
+    cout << nums[k-1];
+    // Please write your code here.
+
+    return 0;
+}
