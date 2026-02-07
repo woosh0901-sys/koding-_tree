17f1c599b4b6b13b43e4f4889d0fc95690bff391
WooSeoHyun <woosh0901@gmail.com>
2026-02-07 13:51:24 +0900

    Title: 최대로 겹치는 구간, Time: 4ms, Memory: 0MB, Status: Passed - Codetree

--- 260207/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 260207/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -7,33 +7,20 @@ int x1[100], x2[100];
 
 int main() {
     cin >> n;
+    int ans[201] = {0,};
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
-    }
+        x1[i] += 100;
+        x2[i] += 100;
 
-    for(int i = 0; i < n; i++){
-        if(x1[i] || x2[i] < 0) {
-            if(x1[i] < x2[i]){
-                x1[i] += -1 * x1[i];
-                x2[i] += -1 * x1[i];
-            }
-            else{
-                x1[i] += -1 * x2[i];
-                x2[i] += -1 * x2[i];
-            }
-        }
-    }
-
-    int ans[101] = {0,};
-    for(int i = 0; i < n; i++){
-        for(int j = x1[i]; j < x2[i] -1; j++){
+        for (int j = x1[i]; j < x2[i]; j++) {
             ans[j]++;
         }
     }
 
     int max = ans[0];
-    for(int i = 1; i<= n; i++){
+    for(int i = 0; i<= 200; i++){
         if(ans[i] > max) max = ans[i];
     }
 
