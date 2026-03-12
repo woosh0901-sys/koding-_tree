962e01ad7cbf9357fd7b6a771ed402aa3ffc7c91
WooSeoHyun <woosh0901@gmail.com>
2026-03-12 17:59:36 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260312/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c - sc.nextInt();
+        double avg = (a+b)/3.0;
+        System.out.println(a+b);
+        System.out.println(avg);
+        System.out.println(a+b-avg);  // Please write your code here.
+    }
+}
