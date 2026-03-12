e9b811b55ea085c2eea8254ca313283d39f8456c
WooSeoHyun <woosh0901@gmail.com>
2026-03-12 18:01:04 +0900

    Title: 합과 평균의 차, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260312/합과 평균의 차/sub-of-average-and-sum.java
+++ 260312/합과 평균의 차/sub-of-average-and-sum.java
@@ -6,9 +6,9 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        double avg = (a+b)/3.0;
-        System.out.println(a+b);
+        int avg = (a+b+c)/3;
+        System.out.println(a+b+c);
         System.out.println(avg);
-        System.out.println(a+b-avg);  // Please write your code here.
+        System.out.println(a+b+c-avg);  // Please write your code here.
     }
 }
