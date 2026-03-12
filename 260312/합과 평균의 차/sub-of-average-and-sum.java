68db23c07bf9c2ef54f68357788187513a8e652b
WooSeoHyun <woosh0901@gmail.com>
2026-03-12 17:59:47 +0900

    Title: 합과 평균의 차, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260312/합과 평균의 차/sub-of-average-and-sum.java
+++ 260312/합과 평균의 차/sub-of-average-and-sum.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c - sc.nextInt();
+        int c = sc.nextInt();
         double avg = (a+b)/3.0;
         System.out.println(a+b);
         System.out.println(avg);
