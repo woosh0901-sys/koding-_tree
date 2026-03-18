f9440b73a7563166e7843cf84d64965f93e42b88
WooSeoHyun <woosh0901@gmail.com>
2026-03-19 08:37:23 +0900

    Title: 음수 구별하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260319/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     
+        int n = scanner.nextInt();
+        
+        if (n > 0) {
+            System.out.println(n);
+        } else {
+            System.out.println("minus");
+        }
+    
+    }
+}
